663bfa731f8a62884ebcedb4d6dec9bd4d7dc513
Abdallah Mahran <eng.abdallah.mahran@gmail.com>
2017-09-24 13:24:20 +0200

    Update QuickSortNew.cpp
    generalize the methods for generic use of the algorithm

--- mycodeschool/QuickSortNew.cpp
+++ mycodeschool/QuickSortNew.cpp
@@ -1,44 +1,45 @@
 #include <iostream>
 
 using namespace std;
-
-void swap(char *a, char *b)
+template<typename T>
+void swap(T *a, T *b)
 {
-	char tmp = *a;
+	T tmp = *a;
 	*a = *b;
 	*b =tmp;
 }
-
-int partition(char arr[], int start, int end)
+template<typename T>
+int partition(T *arr, int start, int end)
 {
-	int x = arr[end];
+	T x = arr[end];
 	int i = start - 1;
 	for(int j = start; j <= end-1; j++)
 	{
 		if(arr[j] <= x)
 		{
 			i = i + 1;
-			swap(&arr[i], &arr[j]);
+			swap<T>(&arr[i], &arr[j]);
 		}
 	}
 	swap(&arr[i+1], &arr[end]);
 	return i+1;
 }
 
-void quickSort(char arr[], int start, int end)
+template<typename T>
+void quickSort(T *arr, int start, int end)
 {
 	if(start<end)
 	{
-		int q = partition(arr, start, end);
-		quickSort(arr, start, q-1);
-		quickSort(arr, q+1, end);
+		int q = partition<T>(arr, start, end);
+		quickSort<T>(arr, start, q-1);
+		quickSort<T>(arr, q+1, end);
 	}
 }
 
 int main()
 {
 	char arr[] = "Lokesh";
-	quickSort(arr, 0, 5);
+	quickSort<char>(arr, 0, 5);
 	cout << arr << endl;
 	return 0;
 }
